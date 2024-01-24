0137e9e1c2ef53058cd8118f3c6dcf83a2096c89
Alejandro Robles <155121406+AleqzzRob@users.noreply.github.com>
2024-01-24 20:16:03 +0000

    Primera clase de python

---
+++ existencia.py
@@ -0,0 +1,12 @@
+class Galleta:
+    def saludar (val):
+        print("soy una galleta")
+        print(val)
+
+oreo=Galleta()
+oreo.saludar("estrella")
+print(oreo)
+
+
+
+    
---
+++ manuel.py
@@ -0,0 +1,13 @@
+class Persona():
+   # name age id
+   def walk ():
+    print("hola soy manuel")
+   def eat ()
+    print("tengo hambre")
+   def talk ():
+    print("tengo sueño")
+
+Persona()=Oscar
+Oscar.walk()
+Oscar.eat()
+Oscar.talk()
